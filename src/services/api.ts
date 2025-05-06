ca178be4e2cbbb7cbd37d0c872cfb56aec169b17
Nuschhz <nusch05@hotmail.com>
2025-05-05 23:12:20 -0300

    chore(api): Changed API endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.NEXT_PUBLIC_ENDPOINT,
   timeout: 10000,
+  headers: {
+    'x-api-key': process.env.NEXT_PUBLIC_SKATE_API,
+  }
 });
 
 export default api;
